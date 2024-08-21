5d869c5fb1068ad6552c003f096b0e19cc7eb7d6
nikhilnetha119 <nikhilkumarthumma119@gmail.com>
2024-08-21 16:40:58 +0530

    Food Recipe App

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import React,{useState} from 'react'
-import TodoList from './Todolist';
-import './App.css';
+import React, { useState } from 'react';
+import Products from './Products';
+import './App.css';  
 
 const App = () => {
-  const [task,setTask] = useState("");
-  const [todos,setTodos] = useState([]);
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+  const YOUR_APP_ID = "4852ece1";
+  const YOUR_APP_KEY = "8883229bb7e0e90fecedb05bd7cc06e4";
 
-  const changeHandler = e =>{
-    setTask(e.target.value)
-  }
-  const submitHandler = e =>{
+  const submitHandler = e => {
     e.preventDefault();
-    const newTodos = [...todos,task];
-    setTodos(newTodos);
-    setTask("");
-  }
-  const deleteHandler = (indexValue) =>{
-    const newTodos = todos.filter((todo,index) => index !== indexValue);
-    setTodos(newTodos);
-  }
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`)
+      .then(response => response.json())
+      .then(data => setData(data.hits));
+  };
+
   return (
     <div>
       <center>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Todo Management Application</h5>
-            <form onSubmit={submitHandler}>
-              <input type="text" name="task" value={task} onChange={changeHandler} /> &nbsp;&nbsp;
-              <input type="submit" value="Add" name="Add"/>
-            </form>
-            <TodoList todolist={todos} deleteHandler={deleteHandler}/>
-          </div>
-        </div>
+        <h1>Food Recipe App</h1>
+        <form onSubmit={submitHandler}>
+          <input 
+            type="text" 
+            value={search} 
+            onChange={(e) => setSearch(e.target.value)} 
+            placeholder="Search for a recipe..." 
+          /><br /><br />
+          <input type="submit" className="btn btn-primary" value="Search" />
+        </form>
+        {data.length >= 1 ? <Products data={data} /> : <h3>No Results Found</h3>}
       </center>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
